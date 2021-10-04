b224e13c7204b15ac5e36d1f5ed688e05adf5534
Abubakar Khawaja <abubakar.khawaja@arbisoft.com>
2021-10-04 10:32:11 +0500

    load more functionality aded

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,19 @@ import Table from './Table';
 
 export default function Home() {
   const [items, setItems] = useState([]);
+  const [hasNextPage, setHasNextPage] = useState(false);
+  let [page, setPage] = useState(1);
+  let [perPage, setPerPage] = useState(
+    parseInt(process.env.REACT_APP_PER_PAGE)
+  );
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [page, perPage]);
 
   async function getData() {
     const curated = await fetch(
-      `https://api.pexels.com/v1/curated?page=1&per_page=${process.env.REACT_APP_PER_PAGE}`,
+      `https://api.pexels.com/v1/curated?page=${page}&per_page=${perPage}`,
       {
         headers: new Headers({
           Authorization: process.env.REACT_APP_API_KEY,
@@ -29,8 +34,26 @@ export default function Home() {
       });
 
     if (curated !== undefined) {
+      setHasNextPage(curated.next_page);
+      if (items.length !== 0) {
+        setItems([...items, ...curated.photos]);
+        return curated;
+      }
       setItems(curated.photos);
     }
   }
-  return <Table items={items} label='Trending' />;
+
+  function loadMore() {
+    setPage(page + 1);
+  }
+  return (
+    <>
+      <Table items={items} label='Trending' />
+      {hasNextPage && (
+        <button type='button' onClick={loadMore}>
+          Load More
+        </button>
+      )}
+    </>
+  );
 }
