58b37ee91f947cccffd289296af84da6a72a84b9
AbubakarKhawaja <abubakar.khawaja@arbisoft.com>
2021-10-08 17:57:50 +0500

    removed unnecessary redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './redux/store';
 import App from './App';
 import './style/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/redux/action/counter.js
+++
@@ -1,9 +0,0 @@
-import { INCREMENT, DECREMENT } from '../types';
-
-export const incrementAction = () => (dispach) => {
-  dispach({ type: INCREMENT });
-};
-
-export const decrementAction = () => (dispach) => {
-  dispach({ type: DECREMENT });
-};
