9cb3e49f4a9ca660348047a1905a779fa2141237
AbubakarKhawaja <abubakar.khawaja@arbisoft.com>
2021-11-03 12:29:24 +0500

    video play error fixed and made interaction tab generic

--- src/components/PhotoView.js
+++ src/components/PhotoView.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
-import { saveAs } from 'file-saver';
 import { useMedia } from '../hooks/useMedia';
 import { likePhoto, unlikePhoto } from '../actions/actions';
-import { HEART_SVG, ADD_SVG } from '../assets/logo';
+import { interactions } from '../services/utils';
 import './PhotoView.css';
 
 function PhotoView({ match }) {
@@ -33,25 +32,9 @@ function PhotoView({ match }) {
       <div className='header'>
         <div className='photographer'>{media && media.photographer}</div>
 
-        <div className='interactions'>
-          <button onClick={likeEvent}>
-            {HEART_SVG}
-            Like
-          </button>
-          <button>
-            {ADD_SVG}
-            Add
-          </button>
-
-          <button
-            onClick={() => {
-              saveAs(media.src.original, media.id);
-            }}
-          >
-            Download
-          </button>
-        </div>
+        {interactions(likeEvent, media, media.src)}
       </div>
+
       {media && (
         <img
           className='image-view'
--- src/components/VideoView.js
+++ src/components/VideoView.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
-import { saveAs } from 'file-saver';
 import { useMedia } from '../hooks/useMedia';
 import { likeVideo, unlikeVideo } from '../actions/actions';
-import { HEART_SVG, ADD_SVG } from '../assets/logo';
+import { interactions } from '../services/utils';
 import './PhotoView.css';
 
 function VideoView({ match }) {
@@ -21,46 +20,37 @@ function VideoView({ match }) {
         pathname: '/login',
       });
     }
-
     if (user.videos?.indexOf(media.id) !== -1) {
       dispatch(unlikeVideo(media.id));
       return;
     }
     dispatch(likeVideo(media.id));
   }
+
   return (
     <>
       <div className='header'>
         <div className='photographer'>{media?.user?.name}</div>
 
-        <div className='interactions'>
-          <button onClick={likeEvent}>
-            {HEART_SVG}
-            Like
-          </button>
-          <button>
-            {ADD_SVG}
-            Add
-          </button>
-
-          <button
-            onClick={() => {
-              saveAs(media.video_files[1].link, media.id);
-            }}
-          >
-            Download
-          </button>
-        </div>
+        {interactions(likeEvent, media, media?.video_files)}
       </div>
 
       <div className='player-wrapper'>
-        {media && (
+        {media.video_files && (
           <video
             className='react-player'
             key={media.id}
-            src={media ?? media.video_files[2].link}
+            src={media.video_files[2].link}
             poster={media.image}
-            onMouseOver={(event) => event.target.play()}
+            onMouseOver={(event) => {
+              var playPromise = event.target.play();
+
+              if (playPromise !== undefined) {
+                playPromise.then((_) => {}).catch((error) => {});
+              } else {
+                console.error('Cant play video');
+              }
+            }}
             onMouseOut={(event) => event.target.pause()}
             muted
             loop
